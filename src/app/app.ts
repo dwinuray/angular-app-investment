5bc73bc7e091a9575e3508c7ca9f262de1bb6c5d
Dwi Nur Cahyo <dwinuray.dev@gmail.com>
2026-08-23 20:54:16 +0700

    using signal and reset form

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,14 +13,23 @@ import { InvestmentResults } from './investment-results/investment-results';
 })
 export class App {
   protected readonly title = signal('investment-calculator');
-  resultsData?: {
+  // resultsData?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number
+  // }[];
+
+  resultsData = signal<{
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment: number,
     totalInterest: number,
     totalAmountInvested: number
-  }[];
+  }[] | undefined>(undefined);
 
   onCalculateInvestmentResults(data: InvestmentInput) {
 
@@ -44,6 +53,7 @@ export class App {
       });
     }
 
-    this.resultsData = annualData;
+    // this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
--- src/app/user-input/user-input.ts
+++ src/app/user-input/user-input.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, output } from '@angular/core';
+import { Component, EventEmitter, Output, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from '../investment-input.model';
 
@@ -13,17 +13,22 @@ export class UserInput {
   @Output() calculate = new EventEmitter<InvestmentInput>();
 
   // properties 
-  initialInvestment = '0';
-  annualInvestment = '0';
-  expectedReturn = '5';
-  duration = '10';
+  initialInvestment = signal('0');
+  annualInvestment = signal('0');
+  expectedReturn = signal('5');
+  duration = signal('10');
 
   onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.initialInvestment,
-      duration: +this.duration,
-      expectedReturn: +this.expectedReturn,
-      annualInvestment: +this.annualInvestment
-    })
+      initialInvestment: +this.initialInvestment(),
+      duration: +this.duration(),
+      expectedReturn: +this.expectedReturn(),
+      annualInvestment: +this.annualInvestment()
+    });
+
+    this.initialInvestment.set('0');
+    this.annualInvestment.set('0');
+    this.expectedReturn.set('50');
+    this.duration.set('10');
   }
 }
