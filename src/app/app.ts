6f6df0883dc6c040a80163b8b069b5ed699de080
Dwi Nur Cahyo <dwinuray.dev@gmail.com>
2026-08-23 17:11:40 +0700

    Init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Table } from './table/table';
+import { Formestimate } from './formestimate/formestimate';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Table, Formestimate],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
